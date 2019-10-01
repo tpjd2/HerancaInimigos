0592a5c96c2d08327ce3ba5840e3f06d6fe4b422
DESKTOP-OUVJOVR\Salmo <salmo.jr@hotmail.com>
2019-10-01 11:31:10 -0300

    implementação do tiro dos inimigos

---
+++ MovePlayerRPG/Assets/Scripts/FireController.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireController : MonoBehaviour
+{
+    [SerializeField] float speed;
+    Vector3 target;
+    
+    void Update()
+    {
+        transform.position = Vector3.MoveTowards(
+            transform.position,
+            target,
+            speed * Time.deltaTime
+            );
+    }
+
+    public void setTarget(Vector3 target)
+    {
+        this.target = (target - transform.position) * 100f;
+    }
+
+    private void OnBecameInvisible()
+    {
+        Destroy(gameObject);
+    }
+}
--- MovePlayerRPG/Assets/Scripts/NonPlayerCharacterController.cs
+++ MovePlayerRPG/Assets/Scripts/NonPlayerCharacterController.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public abstract class NonPlayerCharacterController : MonoBehaviour
 {
+    [SerializeField] protected GameObject firePrefab;
     [SerializeField] protected float speed;
     protected Animator anim;
     // Start is called before the first frame update
     protected abstract void Move();
     protected abstract void UpdateAnimation();
+    protected void Fire()
+    {
+        GameObject fire = (GameObject)Instantiate(firePrefab,transform.position, Quaternion.identity);
+        fire.GetComponent<FireController>().setTarget(GameController.instancia.getPlayer().position);        
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag("Player"))
+        {
+            Fire();
+        }
+    }
 }
